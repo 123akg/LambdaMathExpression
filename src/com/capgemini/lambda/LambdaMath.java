5645cf46aaef62f53b6472019b9f71154dee3b83
123akg <abhijeetkumargiri7070@gmail.com>
2020-10-01 00:28:46 +0530

    Use Case 4 Added

--- src/com/capgemini/lambda/LambdaMath.java
+++ src/com/capgemini/lambda/LambdaMath.java
@@ -2,6 +2,8 @@ package com.capgemini.lambda;
 
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Function;
+
 
 class MyConsumer implements Consumer<Integer>{
 	public void accept(Integer t) {
@@ -12,39 +14,13 @@ class MyConsumer implements Consumer<Integer>{
 
 public class LambdaMath {
 	
-	 public static void main(String[] args) {
+	public static void main(String[] args) {
 		  
 		  ArrayList<Integer> numbers= new ArrayList<Integer>(Arrays.asList(23,28,98,47,23,12,24));
-		  
-		  //Method 1
-		  Iterator<Integer> it=numbers.iterator();
-		  while(it.hasNext()) {
-			  Integer i=it.next();
-			  System.out.println("Iterator value::"+i);
-		  }
-		  
-		  //Method 2...
-		  MyConsumer myconsumer=new MyConsumer();
-		  numbers.forEach(myconsumer);
-		  
-		  
-		  //Method 3..
-		  numbers.forEach(new Consumer<Integer>() {
-			  public void accept(Integer t) {
-				  System.out.println("Method 3 for anonymous class : "+t);
-			  }
-		  });
-		  
-		  //Method 4(Explicit Lambda)
-		  Consumer<Integer> myListAction=n->{
-			  System.out.println("Method 4:"+n);
-		  };
-		  numbers.forEach(myListAction);
-		  
-		  //Method 5(Implicit Lambda)
+		  Function<Integer,Double> doubleFunction=Integer->(Double.valueOf(Integer));
 		  numbers.forEach(n->{
-			  System.out.println("Method 5: for each numbers"+n);
+			  System.out.println("Applying double function "+doubleFunction.apply(n));
 		  });
-		  		  
+		  
 	   }
 }
