b155f9af03907e5c22dadbe14a897969f3e04814
123akg <abhijeetkumargiri7070@gmail.com>
2020-10-01 00:31:33 +0530

    Use Case 5 Added

--- src/com/capgemini/lambda/LambdaMath.java
+++ src/com/capgemini/lambda/LambdaMath.java
@@ -1,25 +1,18 @@
 package com.capgemini.lambda;
 
 import java.util.*;
-import java.util.function.Consumer;
-import java.util.function.Function;
-
-
-class MyConsumer implements Consumer<Integer>{
-	public void accept(Integer t) {
-		System.out.println("Method2 value::"+t);
-	}
-}
+import java.util.function.Predicate;
 
 
 public class LambdaMath {
 	
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 		  
 		  ArrayList<Integer> numbers= new ArrayList<Integer>(Arrays.asList(23,28,98,47,23,12,24));
-		  Function<Integer,Double> doubleFunction=Integer->(Double.valueOf(Integer));
+		  Predicate<Integer> isEvenFunction=n->n%2==0;
+		  
 		  numbers.forEach(n->{
-			  System.out.println("Applying double function "+doubleFunction.apply(n));
+			  System.out.println("Applying isEvenfunction on  "+n+"  and result is :"+isEvenFunction.test(n));
 		  });
 		  
 	   }
